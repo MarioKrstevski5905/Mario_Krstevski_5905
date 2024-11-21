4ec80f1425850ef5658e3733c99bd4f88b8b7641
student <student@uacs>
2024-11-21 18:09:49 +0100

    C0miit

---
+++ Lecture8/Exercise1/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    private String s;
+    public Test(){
+        s = "Programming languages";
+    }
+    public void method1(){
+        System.out.println("Value of s is " + s);
+    }
+    public void method2(int i){
+        System.out.println("Value of i is" + i);
+    }
+    private void method3(){
+        System.out.println("");
+    }
+
+}
